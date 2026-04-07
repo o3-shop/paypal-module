0766ec7c257c453045673c0252f10d9e57d2812c
nick <nlo@tro.net>
2026-04-07 10:04:46 +0200

    todo: add todo for BN Code.

--- src/Core/Constants.php
+++ src/Core/Constants.php
@@ -57,6 +57,8 @@ class Constants
     public const PAYPAL_MAXIMUM_TIME_FOR_CAPTURE = 29 * self::PAYPAL_DAY;
 
     // BN Codes defined together with PayPal
+
+    // @todo: change these to our own BN Codes
     public const PAYPAL_PARTNER_ATTRIBUTION_ID_PPCP = 'OXID_Cart_PPCP_fromv261';
     // deprecated, we use only the PPCP-BN-Code since now 2023-11
     public const PAYPAL_PARTNER_ATTRIBUTION_ID_EXPRESS = 'Oxid_Cart_PymtsShortcut';
